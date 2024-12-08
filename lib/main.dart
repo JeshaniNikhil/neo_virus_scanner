9ac9b65a76cb991a54b70db8d0e99c9cfc49cdca
unadkatmadhav <unadkatmadhav26@gmail.com>
2024-12-08 13:52:26 +0530

    hello class created

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:virus_scanner/Login%20credentials/Login.dart';
-import 'splash screen data/splash_screen.dart';
 
 void main() {
   
