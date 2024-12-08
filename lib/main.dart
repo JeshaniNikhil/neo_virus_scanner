02f5832a690288a7d55c4ea590e034fd6fdf4287
unadkatmadhav <unadkatmadhav26@gmail.com>
2024-12-08 15:40:34 +0530

    created constant file

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:virus_scanner/Login/Login.dart';
 import 'package:virus_scanner/Register/Register.dart';
-import 'file_upload.dart';
+import 'uploads/file_upload.dart';
 void main() {
   WidgetsFlutterBinding.ensureInitialized(); // Ensure bindings are initialized
   runApp(
