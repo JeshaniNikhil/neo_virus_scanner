0d4c2597e0de5ac3aa7fac544a21de7e1f041fac
Nikhil Jeshani <140085318+JeshaniNikhil@users.noreply.github.com>
2024-12-08 14:54:17 +0530

    `Removed Login widget and updated main.dart to use FileUploadScreen instead`

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:virus_scanner/Login%20credentials/Login.dart';
-
+import 'package:virus_scanner/Login_credentials/Login.dart';
+import 'file_upload.dart';
 void main() {
   
   runApp(
     MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Login(),
+      home: FileUploadScreen(),
       theme: ThemeData(
         textTheme: GoogleFonts.interTextTheme(),
       ),
