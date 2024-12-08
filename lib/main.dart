49a10943095eca9a9c56115e6306c8172e419e68
Nikhil Jeshani <140085318+JeshaniNikhil@users.noreply.github.com>
2024-12-08 15:31:54 +0530

    `Added Register page and updated main.dart to point to it`

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:virus_scanner/Login_credentials/Login.dart';
+import 'package:virus_scanner/Login/Login.dart';
+import 'package:virus_scanner/Register/Register.dart';
 import 'file_upload.dart';
 void main() {
-  
+  WidgetsFlutterBinding.ensureInitialized(); // Ensure bindings are initialized
   runApp(
     MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: FileUploadScreen(),
+      home: Register(),
       theme: ThemeData(
         textTheme: GoogleFonts.interTextTheme(),
       ),
